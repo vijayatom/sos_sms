a5a7871efb0ee66e0b16f26d6730c7a456a97412
Vijay <96625882+vijayatom@users.noreply.github.com>
2023-05-19 12:25:14 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,15 +1,3 @@
-# import os
-# from twilio.rest import Client
-
-# account_sid = "AC5f879e1950492316bd67150703f0aee2"
-# auth_token = "84385c74b21d11aa856f9ee64e74d1c9"
-# client = Client(account_sid, auth_token)
-# message = client.messages.create(
-#   body="(🚨🚨🚨I am in Trouble🚨🚨🚨)",
-#   from_="+13204139325",
-#   to="+919360214003"
-# )
-# print(message.sid)
 
 import streamlit as st
 from twilio.rest import Client
